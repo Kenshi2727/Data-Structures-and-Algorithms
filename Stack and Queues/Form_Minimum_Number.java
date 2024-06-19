229f172e913b8deb5549b97f0bfe3db34caca052
Kenshi2727 <abhishekmathurofficial@gmail.com>
2024-06-19 18:15:51 +0530

    valid subarrays

--- Stack and Queues/Form_Minimum_Number.java
+++ Stack and Queues/Form_Minimum_Number.java
@@ -8,8 +8,9 @@ public static void main(String[] args) {
     Stack<Integer> stack = new Stack<>();
     for(int i=0;i<=str.length();i++){
         stack.push(i+1);
-        if(i==str.length() || str.charAt(i)=='I'){ //str.charAt(i)=='I' means we will pop all the elements from the stack till we get the I
-            while(!stack.isEmpty()){
+         //str.charAt(i)=='I' means we will pop all the elements from the stack till we get the I
+        if(i==str.length() || str.charAt(i)=='I'){//DO NOT REVERSE THE CONDITION ELSE IT WILL GIVE ERROR AS STRING INDEX OUT OF BOUND EXCEPTION
+            while(!stack.isEmpty()){              //YOU CAN CHECK FOR YIURSELF BY REVERSING THE CONDITION AND THEN RUNNING THE CODE FOR INPUT="II"  
                 System.out.print(stack.pop());
                 
             }
